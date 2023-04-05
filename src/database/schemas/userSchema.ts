1f958216dd27894908dfdb769b08e1bd953ac79f
matheustkaczyk <matheus111904@hotmail.com>
2023-04-05 14:41:09 -0300

    feat: created UserSchema, exporting user model

---
+++ src/database/schemas/userSchema.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document } from "mongoose";
+import IUser from "../../interfaces/IUser";
+
+export interface IUserDocument extends IUser, Document {}
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+});
+
+export const User = mongoose.model<IUserDocument>("User", userSchema);
