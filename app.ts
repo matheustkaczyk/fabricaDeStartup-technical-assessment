2f240b92f517862c72fc8bcfdf5dd66e2dae81d6
matheustkaczyk <matheus111904@hotmail.com>
2023-04-05 14:02:50 -0300

    feat: instantiating database class, calling database on start method

--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
 import express, { Application } from "express";
+import Database from "./src/database/database";
 
 export default class App {
   private app: Application;
   private port: number;
+  private db: Database;
 
   constructor() {
     this.app = express();
     this.port = Number(process.env.PORT) || 3000;
+    this.db = new Database();
   }
 
-  public start() {
+  public async start() {
+    await this.db.connect();
     this.app.listen(this.port, () => {
       console.log(`Server started on port ${this.port}`);
     });
