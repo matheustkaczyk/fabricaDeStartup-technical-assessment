89bed9ca4d26bacaa60e0571c7d785b3f4d60f67
matheustkaczyk <matheus111904@hotmail.com>
2023-04-05 14:48:08 -0300

    feat: created jsonwebtoken class

---
+++ src/validations/jwt.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export default class Jwt {
+    private secret: string;
+
+    constructor(secret: string) {
+        this.secret = secret || process.env.SECRET as string;
+    }
+
+    public sign(payload: any): string {
+        return jwt.sign(payload, this.secret);
+    }
+
+    public verify(token: string): any {
+        return jwt.verify(token, this.secret);
+    }
+}
