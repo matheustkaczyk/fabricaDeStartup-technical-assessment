aa2b90c49ba16b6651650e137168ad842584f280
matheustkaczyk <matheus111904@hotmail.com>
2023-04-06 14:58:15 -0300

    feat: adding config method on App, it calls express.json

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express, { Application } from "express";
+
 import Database from "./database";
+
+import { UserController } from "./controllers/userController";
 
 export default class App {
   private app: Application;
   private port: number;
   private db: Database;
+  private userController: UserController;
 
   constructor() {
     this.app = express();
+    this.config();
     this.port = Number(process.env.PORT) || 3000;
     this.db = new Database();
+    this.userController = new UserController();
+    this.setupRoutes();
   }
 
   public async start() {
@@ -17,5 +24,13 @@ export default class App {
     this.app.listen(this.port, () => {
       console.log(`Server started on port ${this.port}`);
     });
+  }
+
+  private config() {
+    this.app.use(express.json());
+  }
+
+  private setupRoutes() {
+    this.app.post("/auth/signup", this.userController.createUser.bind(this.userController));
   }
 }
