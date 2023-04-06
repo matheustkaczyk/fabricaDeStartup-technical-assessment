17a36762519b3a2b2d14f47ae184bf98de24b13d
matheustkaczyk <matheus111904@hotmail.com>
2023-04-06 11:56:29 -0300

    feat: userModel findUser method

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,3 +1,15 @@
-export class UserModel {
+import { User } from "../database/schemas/userSchema";
 
+import IUser from "../interfaces/IUser";
+
+export class UserModel {
+    async findUser(email: string): Promise<IUser | null> {
+        const userFound = await User.findOne({ email });
+
+        if (userFound) {
+            return userFound;
+        }
+
+        return null;
+    }
 }
