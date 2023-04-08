315d430164f944bcad5bd683c3bcf0471b86ce84
matheustkaczyk <matheus111904@hotmail.com>
2023-04-08 16:05:01 -0300

    feat: deleteProduct method handling the error correctly

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,5 +1,7 @@
 import { Product } from '../database/schemas/productSchema';
+
 import { CreateProductDto, UpdateProductDto } from '../dto/productDto';
+
 import IProduct from '../interfaces/IProduct';
 
 export class ProductModel {
@@ -29,21 +31,33 @@ export class ProductModel {
     }
 
     async updateProduct(id: string, { name, categories, qty, price }: UpdateProductDto): Promise<void> {
-        const foundProduct = await this.getProductById(id);
+        try {
+            const foundProduct = await this.getProductById(id);
 
-        await Product.findOneAndUpdate({ _id: id }, {
-            name: name || foundProduct.name,
-            categories: categories || foundProduct.categories,
-            qty: qty || foundProduct.qty,
-            price: price || foundProduct.price
-        });
+            await Product.findOneAndUpdate({ _id: id }, {
+                name: name || foundProduct.name,
+                categories: categories || foundProduct.categories,
+                qty: qty || foundProduct.qty,
+                price: price || foundProduct.price
+            });
+        } catch (error) {
+            throw new Error('Product not found');            
+        }
+
     }
 
     async deleteProduct(id: string): Promise<void> {
-        const foundProduct = await Product.findByIdAndDelete(id);
-
-        if (!foundProduct) {
-            throw new Error('Product not found');
-        }
+        try {
+            const foundProduct = await Product.findOneAndRemove({ _id: id });
+        
+            if (!foundProduct) {
+                throw new Error('Product not found');
+            }
+            } catch (err: Error | any) {
+            if (err.name === 'CastError' && err.kind === 'ObjectId') {
+                throw new Error('Invalid product id');
+            }
+            throw err;
+            }
     }
 }
