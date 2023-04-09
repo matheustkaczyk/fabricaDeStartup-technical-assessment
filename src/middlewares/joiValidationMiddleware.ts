7c03121f7a0c933d8e885bdb166b69205c8164f3
matheustkaczyk <matheus111904@hotmail.com>
2023-04-09 09:27:33 -0300

    refact: refactoring joi middleware

--- src/middlewares/joiValidationMiddleware.ts
+++ src/middlewares/joiValidationMiddleware.ts
@@ -1,19 +1,15 @@
-import Joi, { ObjectSchema } from 'joi';
+import { ObjectSchema } from 'joi';
 import { Request, Response, NextFunction } from 'express';
 
 export default class JoiValidationMiddleware {
-    private schema: ObjectSchema;
-
-    constructor(schema: ObjectSchema) {
-        this.schema = schema;
-    }
-
-    validate(req: Request, res: Response, next: NextFunction) {
-        const { error } = this.schema.validate(req.body);
-        if (error) {
-            res.status(400).send(error.details[0].message);
-        } else {
-            next();
+    validate(schema: ObjectSchema) {
+        return (req: Request, res: Response, next: NextFunction) => {
+            const { error } = schema.validate(req.body);
+            if (error) {
+                res.status(400).json({ error: error.details[0].message });
+            } else {
+                next();
+            }
         }
     }
 }
