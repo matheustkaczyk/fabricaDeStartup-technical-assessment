b01ef9b5e853105b3a07893830e462921a4a8b2d
matheustkaczyk <matheus111904@hotmail.com>
2023-04-09 09:21:28 -0300

    refact: refactoring userSchema messages, wasn't working

--- src/validations/joi/userSchema.ts
+++ src/validations/joi/userSchema.ts
@@ -1,10 +1,38 @@
 import Joi from 'joi';
 
 export const userSchema = Joi.object({
-    email: Joi.string().email().required().message('Email must be a valid email'),
-    password: Joi.string().min(6).max(30).required().message('Password must be between 6 and 30 characters'),
-    name: Joi.string().min(3).max(30).required().message('Name must be between 3 and 30 characters'),
-    type: Joi.string().valid('admin', 'user').required().message('Type must be either admin or user'),
+    email: Joi.string()
+    .email()
+    .required()
+    .messages({
+        'string.email': 'Email must be a valid email',
+        'string.empty': 'Email is required',
+    }),
+    password: Joi.string()
+    .min(6)
+    .max(30)
+    .required()
+    .messages({
+        'string.min': 'Password must be at least 6 characters',
+        'string.max': 'Password must be at most 30 characters',
+        'string.empty': 'Password is required',
+    }),
+    name: Joi.string()
+    .min(3)
+    .max(30)
+    .required()
+    .messages({
+        'string.min': 'Name must be at least 3 characters',
+        'string.max': 'Name must be at most 30 characters',
+        'string.empty': 'Name is required',
+    }),
+    type: Joi.string()
+    .valid('admin', 'user')
+    .required()
+    .messages({
+        'any.required': 'Type is required',
+        'any.only': 'Type must be either admin or user',
+    }),
 });
 
 export const authenticateUserSchema = Joi.object({
